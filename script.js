f9783ead347612250c71c432b36f6b1eca8cfa41
Abby Benjamin <ablizben@gmail.com>
2021-01-29 09:28:22 -0500

    adding on click fxn for again btn

--- script.js
+++ script.js
@@ -43,3 +43,15 @@ document.querySelector('.check').addEventListener('click', function () {
         }
     }
 });
+
+//on click function for again btn
+document.querySelector('.again').addEventListener('click', function () {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.message').textContent = "Start guessing...";
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+});
